e278956e0debefa70081ab4c3a8ad5846fb054f0
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-01-09 00:12:23 +0530

    complete the JwtService class with creation and validation of token

--- src/main/java/com/example/Student/management/system/Security/JwtService.java
+++ src/main/java/com/example/Student/management/system/Security/JwtService.java
@@ -1,5 +1,38 @@
 package com.example.Student.management.system.Security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+//this is for creation and validation of token
 public class JwtService {
+
+    private final String SECRET_KEY= "mynameisnarutoandthestrongestinthewholeshinibeworld8220";
+
+    // the jwt token was generated here
+    public  String generateToken(UserDetails userDetails){
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+900000))
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    //this is used for token validation
+    public String extractUsername(String token){
+
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY.getBytes())
+                .build().parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
 
 }
