e1bb3af867277094e3873ac69b2fa5ced9f4123c
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-01-11 00:38:14 +0530

    add the code in the UserLoginRequest dto

--- src/main/java/com/example/Student/management/system/dto/UserLoginRequest.java
+++ src/main/java/com/example/Student/management/system/dto/UserLoginRequest.java
@@ -1,4 +1,15 @@
 package com.example.Student.management.system.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class UserLoginRequest {
+
+    @NotNull
+    private String username;
+    @NotNull
+    private String Password;
 }
