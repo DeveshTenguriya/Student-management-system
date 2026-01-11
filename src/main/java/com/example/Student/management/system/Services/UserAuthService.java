81261fb2d8e348a8d2310a74b9e5bf4197ea5610
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-01-12 01:18:54 +0530

    passing the user directly which is wrong now save the user in the userrepo then in the userDetail then passing it to the token generation method

--- src/main/java/com/example/Student/management/system/Services/UserAuthService.java
+++ src/main/java/com/example/Student/management/system/Services/UserAuthService.java
@@ -2,7 +2,9 @@ package com.example.Student.management.system.Services;
 
 import com.example.Student.management.system.Repository.UserRepository;
 import com.example.Student.management.system.Security.JwtService;
+import com.example.Student.management.system.Security.UserDetailsServiceImpl;
 import com.example.Student.management.system.dto.UserRegisterRequest;
+import com.example.Student.management.system.entity.Role;
 import com.example.Student.management.system.entity.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,14 +16,14 @@ public class UserAuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final JwtService jwtService;
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
 
-    public UserAuthService(PasswordEncoder passwordEncoder, UserRepository userRepository, JwtService jwtService) {
+    public UserAuthService(PasswordEncoder passwordEncoder, UserRepository userRepository, JwtService jwtService, UserDetailsServiceImpl userDetailsServiceImpl) {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
         this.jwtService = jwtService;
+        this.userDetailsServiceImpl = userDetailsServiceImpl;
     }
-
-
 
     public String register(UserRegisterRequest request){
 
@@ -31,12 +33,14 @@ public class UserAuthService {
 
         User user = new User();
         user.setUsername(request.getUsername());
-
+        user.setRole(Role.USER);
         user.setPassword(passwordEncoder.encode(request.getPassword()));
-
         userRepository.save(user);
 
-        return jwtService.generateToken((UserDetails) user);
+        // 2️⃣ Load UserDetails (for JWT)
+        UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(user.getUsername());
+
+        return jwtService.generateToken(userDetails);
     }
 
 }
